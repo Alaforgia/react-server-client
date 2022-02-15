3807e324a342514ba27d0b973fd8a9d57e5231cd
Tony LaForgia <alforgia428@gmail.com>
2022-02-15 15:15:51 -0600

    added addCreature function

--- src/components/CreatureForm/CreatureForm.jsx
+++ src/components/CreatureForm/CreatureForm.jsx
@@ -1,5 +1,29 @@
+import axios from "axios";
+import { useState } from "react";
+
 function CreatureForm() {
-  return <h2>Add Creature</h2>;
+  const [newCreatureName, setNewCreatureName] = useState("");
+  const [newCreatureOrigin, setNewCreatureOrigin] = useState("");
+  const addCreature = (event) => {
+    event.preventDefault();
+    console.log("Submitted");
+    //     axios.post("/creature")
+    //     .then((response)=>{
+
+    // })
+  };
+
+  return (
+    <>
+      <h2>Add Creature</h2>
+      <form onSubmit={addCreature}>
+        <label htmlFor="name">Name: </label>
+        <input id="name" onChange={(event) => setNewCreatureName(event.target.value)} />
+        <label htmlFor="origin">Origin: </label>
+        <input id="origin" onChange={(event) => setNewCreatureOrigin(event.target.value)} />
+      </form>
+    </>
+  );
 }
 
 export default CreatureForm;
